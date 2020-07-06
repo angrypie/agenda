55bac45e131272a84367560d70ffbf91d2746b71
angrypie <cookies.in.chocolate@gmail.com>
2020-07-07 00:24:58 +0300

    basic styling

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,18 +1,11 @@
 import React from 'react'
-import { Text, View } from 'react-native'
-
-export const Task = ({ name, time }: any) => {
-	return (
-		<View>
-			<Text>{name}</Text>
-			<Text>{time}</Text>
-		</View>
-	)
-}
+import { Text, View, StyleSheet } from 'react-native'
 
 const tasks = [
-	{ name: 'Clean home', time: '12:30' },
-	{ name: 'Pay bills', time: '14:30' },
+	{ name: 'Programming', time: '8:00', active: false },
+	{ name: 'Workout', time: '12:30', active: true },
+	{ name: 'Clean Home', time: '14:00', active: false },
+	{ name: 'Pay Bills', time: '15:00', active: false },
 ]
 
 export const TaskList = () => {
@@ -24,3 +17,35 @@ export const TaskList = () => {
 		</View>
 	)
 }
+
+export const Task = ({ name, time, active }: any) => {
+	const style = { opacity: active ? 1 : 0.5 }
+	return (
+		<View style={[styles.task, style]}>
+			<Header>{name}</Header>
+			<Header>{time}</Header>
+		</View>
+	)
+}
+
+const DefaultText = ({ children, style }: any) => (
+	<Text style={{ color: 'white', ...style }}>{children}</Text>
+)
+
+const Header = ({ children }: any) => (
+	<DefaultText style={{ fontWeight: 'bold', fontSize: 30 }}>
+		{children}
+	</DefaultText>
+)
+
+const styles = StyleSheet.create({
+	task: {
+		minWidth: 280,
+		height: 100,
+		flex: 1,
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		opacity: 1,
+	},
+})
