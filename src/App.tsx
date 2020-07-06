a1684b4ab07c901eb222407b90ead1bbbb02d6a8
angrypie <cookies.in.chocolate@gmail.com>
2020-07-06 18:39:54 +0300

    set resolver to use src directory as base url for import

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, View } from 'react-native'
 
-import { TaskList } from './components/TaskList'
+import { TaskList } from 'components/TaskList'
 
 export default function App() {
 	return (
