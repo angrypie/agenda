7d2cb476aea1965fe2eacb7212226a1f5d0f9259
angrypie <cookies.in.chocolate@gmail.com>
2020-07-25 20:58:54 +0300

    add react-navigation/native with simple stack navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
 import React from 'react'
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, SafeAreaView } from 'react-native'
+import { Button } from 'react-native'
 import { StoreProvider, rootStore } from 'models'
+import { NavigationContainer, DarkTheme } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
 
 import { TaskList } from 'components/TaskList'
+import { DayFocus } from 'components/day-focus'
+
+const Stack = createStackNavigator()
 
 export default function App() {
 	return (
-		<StoreProvider value={rootStore}>
-			<SafeAreaView style={styles.container}>
-				<TaskList />
-				<StatusBar style='auto' />
-			</SafeAreaView>
-		</StoreProvider>
+		<NavigationContainer theme={DarkTheme}>
+			<StoreProvider value={rootStore}>
+				<Stack.Navigator initialRouteName='Focus'>
+					<Stack.Screen
+						name='Focus'
+						component={DayFocus}
+						options={({ navigation }) => ({
+							headerRight: () => (
+								<Button
+									onPress={() => navigation.navigate('Today')}
+									title='Today'
+								/>
+							),
+						})}
+					/>
+					<Stack.Screen name='Today' component={TaskList} />
+				</Stack.Navigator>
+				<StatusBar style='light' />
+			</StoreProvider>
+		</NavigationContainer>
 	)
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#000',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-})
